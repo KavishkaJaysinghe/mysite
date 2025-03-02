f577094dc69b5d48f20041a3992990752807f090
Kavishka Jayasinghe <kavishkajaye@gmail.com>
2025-03-02 06:05:08 +0530

    i3

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import personalIcon from './personalIcon.png'
+import bankingIcon from './bankingIcon.png'
+import posIcon from './posIcon.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    personalIcon,
+    bankingIcon,
+    posIcon,
+    logo
+}
